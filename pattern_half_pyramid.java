7625aa05173d2aa527d541efa049f28eaf0af6ff
Sujit Jamdade <66556255+SujitJamdade@users.noreply.github.com>
2022-06-23 14:56:41 +0530

    Add files via upload

---
+++ pattern_half_pyramid.java
@@ -0,0 +1,29 @@
+package java_class;
+
+import java.util.*;
+public class pattern_half_pyramid {
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+
+        for (int i=1; i<=r; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+Output :
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+ */
