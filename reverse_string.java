665a7321cf37519f5e40ea1fc2c1d1ac3ada451f
Sujit Jamdade <66556255+SujitJamdade@users.noreply.github.com>
2022-06-28 14:23:48 +0530

    Add files via upload

---
+++ reverse_string.java
@@ -0,0 +1,20 @@
+package java_class;
+import java.util.*;
+public class reverse_string {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("hello");
+        System.out.println(sb);
+        for (int i=0; i<sb.length()/2; i++){
+            int front = i;
+            int back = sb.length()-1-i;
+
+            char frontChar = sb.charAt(front);
+            char backChar = sb.charAt(back);
+
+            sb.setCharAt(front, backChar);
+            sb.setCharAt(back, frontChar);
+
+        }
+        System.out.println(sb);
+    }
+}
