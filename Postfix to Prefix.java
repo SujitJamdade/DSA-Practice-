8613a001afa6a3500510b9f7c36d0086c249c599
Sujit Jamdade <66556255+SujitJamdade@users.noreply.github.com>
2022-12-11 14:44:34 +0530

    Stack

---
+++ Postfix to Prefix.java
@@ -0,0 +1,42 @@
+/*
+Postfix to Prefix
+You are given a postfix expression.
+
+You are required to convert it to prefix and print.
+Note
+
+Use brackets in infix expressions to indicate precedence. For a better understanding, check the example.
+
+Input Format
+The first line contains a string s, denoting the postfix expression.
+
+Output Format
+print the prefix expression.
+
+Example 1
+Input
+264*8/+3-
+
+Output
+-+2/*6483
+
+Explanation
+The postfix to prefix expression is printed in the third line.
+
+Example 2
+Input
+12+34*5/6*7/+
+
+Output
+++12/*/*34567
+
+Explanation
+The postfix to prefix expression is printed in the third line.
+
+Constraints
+Expression is a valid prefix expression
+
+The only operators used are +, -, *, /
+
+All operands are single-digit numbers.
+*/
